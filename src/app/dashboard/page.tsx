1618479822aa61d12317166548cae4a8c37fc730
kartik-malviya1 <kartikmalviya30@gmail.com>
2024-10-22 08:57:51 +0530

    dashboard route protected

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,7 +1,18 @@
-export default function dashboard(){
-  return(
-    <div className="w-full h-full">
-      <h1>Dashboard</h1>
+import { auth } from "@clerk/nextjs/server";
+import { redirect } from "next/navigation";
+
+export default async function Dashboard() {
+  const { userId } = await auth();
+
+  // Redirect to the sign-in page if the user is not authenticated
+  if (!userId) {
+    redirect("/sign-in");
+  }
+
+  // If authenticated, render the dashboard content
+  return (
+    <div>
+      <h1>Welcome to your Dashboard</h1>
     </div>
-  ) 
+  );
 }
