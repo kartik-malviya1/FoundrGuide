5e5d264f05c9252d9b5da0e57f46f0b08b12c71f
kartik-malviya1 <kartikmalviya30@gmail.com>
2024-11-06 17:17:09 +0530

    backend separated

--- src/lib/routes/v1/admin.ts
+++
@@ -1,9 +0,0 @@
-import { Router } from "express";
-
-export const adminRouter = Router();
-
-adminRouter.post("//add-book", (req, res) => {
-  // Logic to add a book to the database
-  res.json({ message: "Book added successfully" });
-}) 
-
